2a7aa926e61911947637be75b954aa96f272344c
Mario <developer@theboywhoroared.com>
2020-03-26 19:32:35 -0400

    FIX: Release Candidates must be PRs to master
    This commit fixes a few potential issues with creating
    builds:
    
    - The RC should only be created when the PR is targeting the `master`
      branch.
    
    - Accomodates ad-hoc branches that don't use the `feature/`, `release/`
      etc. prefixes in the branch name.
    
    - Makes it explicit that `master` does not use a pre-release qualifier.
      `master` produces release builds.

--- ci.js
+++ ci.js
@@ -4,6 +4,9 @@ const ci = {
   buildBranch: process.env.BRANCH_NAME || "",
   buildUrl: process.env.BUILD_URL || "",
   buildTag: process.env.BUILD_TAG || "",
+  buildChangeId: process.env.CHANGE_ID || "",
+  buildChangeBranch: process.env.CHANGE_BRANCH || "",
+  buildChangeTarget: process.env.CHANGE_TARGET || "",
   buildCommit:
     process.env.GIT_COMMIT !== "" && process.env.GIT_COMMIT !== undefined
       ? process.env.GIT_COMMIT
@@ -20,28 +23,31 @@ const ci = {
   preReleaseLabel: function () {
     const branch = this.buildBranch.toLowerCase();
     const isLocal = !branch || !this.buildNumber;
-    
+    const isPullRequest = this.buildChangeId !== "";
+    const isRelease = this.buildBranch === "master";
+    const isReleaseCandidate =
+      isPullRequest &&
+      this.buildChangeTarget === "master" &&
+      (this.buildChangeBranch.indexOf("release/") !== -1 ||
+        this.buildChangeBranch.indexOf("hotfix/") !== -1);
+
     if (isLocal) {
       // Developers won't usually set these environment variables; these are
       // usually available when we run under Jenkins CI
       return "-local";
     }
 
-    const isReleaseCandidate =
-      (branch.indexOf("release/") || branch.indexOf("hotfix/")) &&
-      process.env.CHANGE_ID;
-    const isFeature = branch.indexOf("feature/") > -1;
+    if (isRelease) {
+      return "";
+    }
 
     if (isReleaseCandidate) {
       return `-rc${this.buildNumber}`;
     }
 
-    if (isFeature) {
-      const name = branch.split("/")[1];
-      return `-${name}.${this.buildNumber}.${this.buildCommitShort}`;
-    }
+    const name = branch.indexOf("/") !== -1 ? branch.split("/")[1] : branch;
 
-    return ""
+    return `-${name}.${this.buildNumber}.${this.buildCommitShort}`;
   },
 };
 
